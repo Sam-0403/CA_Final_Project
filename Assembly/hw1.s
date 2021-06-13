20a693436c5b0e9177947b01c11df9efda4415ca
Sam-0403 <83916396+Sam-0403@users.noreply.github.com>
2021-06-13 13:02:44 +0800

    Add files via upload

--- Assembly/hw1.s
+++ Assembly/hw1.s
@@ -6,7 +6,25 @@
 
 FUNCTION:
     # Todo: Define your own function in HW1
-    
+    addi a5, x10, 0
+func:
+    addi sp, sp, -8
+    sw   x1, 4(sp)  #
+    sw   a5, 0(sp)  #
+    addi a1, a5, -1 
+    bne  a1, x0, L1
+    addi x10, x0, 7
+    addi sp, sp, 8
+    jalr x0, 0(x1)
+L1: srli a5, a5, 1
+    jal  x1, func
+    lw   a5, 0(sp)
+    lw   x1, 4(sp)
+    addi sp, sp, 8
+    slli x10, x10, 3
+    slli t1, a5, 2
+    add x10, x10, t1
+    jalr x0, 0(x1)
 
 # Do NOT modify this part!!!
 __start:
